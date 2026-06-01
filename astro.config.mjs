809971d8bee2e7f4fd107d5fff3c1f47fc980218
astrotobby <chanzeratobias@gmail.com>
2026-06-01 20:33:45 +0300

    Refactor font configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
@@ -10,38 +10,30 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
 
   fonts: [
-      {
-          provider: fontProviders.local(),
-          name: 'Atkinson',
-          cssVariable: '--font-atkinson',
-          fallbacks: ['sans-serif'],
-          options: {
-              variants: [
-                  {
-                      src: ['./src/assets/fonts/atkinson-regular.woff'],
-                      weight: 400,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-                  {
-                      src: ['./src/assets/fonts/atkinson-bold.woff'],
-                      weight: 700,
-                      style: 'normal',
-                      display: 'swap',
-                  },
-              ],
+    {
+      provider: fontProviders.local(),
+      name: 'Atkinson',
+      cssVariable: '--font-atkinson',
+      fallbacks: ['sans-serif'],
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/atkinson-regular.woff'],
+            weight: 400,
+            style: 'normal',
+            display: 'swap',
           },
+          {
+            src: ['./src/assets/fonts/atkinson-bold.woff'],
+            weight: 700,
+            style: 'normal',
+            display: 'swap',
+          },
+        ],
       },
-		],
+    },
+  ],
 
-({
-    platformProxy: {
-      enabled: false
-    }
-  }),
-  image: {
-    service: { entrypoint: 'astro/assets/services/noop' }
-  },
   vite: {
     server: {
       allowedHosts: true,
