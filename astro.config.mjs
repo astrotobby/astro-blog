262d4971600cd40c9c03589a6eb4efe2c2390c94
astrotobby <2.08241161e+08+astrotobby@users.noreply.github.com>
2026-06-07 08:37:15 +0000

    fix: resolve deployment errors and cleanup empty posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
   adapter: cloudflare(),
   site: 'https://astrotobby.site',
   integrations: [mdx(), sitemap({ entryLimit: 10000 })],
