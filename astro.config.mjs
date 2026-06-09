2e165aab9d506b5881111575fb88b11fc427e3bf
astrotobby <2.08241161e+08+astrotobby@users.noreply.github.com>
2026-06-09 04:35:39 +0000

    fix: enable cloudflare adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  // // adapter: cloudflare(),
+  output: "server",
+  adapter: cloudflare(),
   site: 'https://astrotobby.site',
   integrations: [mdx(), sitemap({ entryLimit: 10000 })],
 
